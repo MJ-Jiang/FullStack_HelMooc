8b208c72045edd3aee05149efb22f00a5f2b68c2
MJ-Jiang <maura_jiang@outlook.com>
2025-06-17 14:36:56 +0300

    4.1.-4.2.

---
+++ Part4/bloglist/app.js
@@ -0,0 +1,14 @@
+// app.js
+const express = require('express')
+const mongoose = require('mongoose')
+const config = require('./utils/config')
+const blogsRouter = require('./controllers/blog')
+
+const app = express()
+
+mongoose.connect(config.MONGODB_URI)
+
+app.use(express.json())
+app.use('/api/blogs', blogsRouter)
+
+module.exports = app
