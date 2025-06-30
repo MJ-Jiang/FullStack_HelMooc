08e2143213b33fa9eb0460ec8e162b7aaf67941f
MJ-Jiang <maura_jiang@outlook.com>
2025-06-30 15:38:33 +0300

    5.8

--- Part5/bloglist-frontend/src/components/Blog.jsx
+++ Part5/bloglist-frontend/src/components/Blog.jsx
@@ -1,16 +1,34 @@
 import{useState} from 'react'
 import '../App.css'
+import blogService from '../services/blogs'
 
 
 const Blog = ({ blog }) => {
   const [showDetails, setShowDetails] = useState(false)
-
+  const [likes, setLikes] = useState(blog.likes)
   const blogStyle = {
     paddingTop: 10,
     paddingLeft: 2,
     borderWidth: 1,
     marginBottom: 5
   }
+  const handleLike=async()=>{
+  try{
+    const updatedBlog={
+      title:blog.title,
+      author:blog.author,
+      url:blog.url,
+      likes:blog.likes+1,
+      user:blog.user
+      
+    }
+    const returnedBlog=await blogService.update(blog.id,updatedBlog)
+    setLikes(returnedBlog.likes)
+
+  }catch(error){
+    console.error('Error liking the blog:', error)
+  }
+}
 
   return(
       <div style={blogStyle}>
@@ -26,9 +44,9 @@ const Blog = ({ blog }) => {
         <p>{blog.url}</p>
         <div>
           <p style={{ display: 'inline', marginRight: '10px' }}>
-          likes {blog.likes}
+          likes {likes}
           </p>
-          <button className='button'>like</button>
+          <button className='button' onClick={handleLike}>like</button>
         </div>
         <p>{blog.author}</p>
         </div>
