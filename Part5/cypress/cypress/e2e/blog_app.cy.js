2891b4dc4c1ad48d0c9ac13a2c86c0b93b82f136
MJ-Jiang <maura_jiang@outlook.com>
2025-07-05 22:01:42 +0300

    5.19-5.22

--- Part5/cypress/cypress/e2e/blog_app.cy.js
+++ Part5/cypress/cypress/e2e/blog_app.cy.js
@@ -1,15 +1,15 @@
 describe('Blog app', function() {
   beforeEach(function() {
-    cy.request('POST', 'http://localhost:3001/api/testing/reset')
+    cy.request('POST', `${Cypress.env('BACKEND')}/testing/reset`)
     const user = {
       name: 'June',
       username: 'June',
       password: 'salainen'
     }
-    cy.request('POST', 'http://localhost:3001/api/users/', user)
-
-    cy.visit('http://localhost:5173')
+    cy.request('POST', `${Cypress.env('BACKEND')}/users`, user)
+    cy.visit('')
   })
+  
   it('Login form is shown', function() {
     cy.get('#username').should('be.visible')
     cy.get('#password').should('be.visible')
@@ -21,15 +21,76 @@ describe('Blog app', function() {
       cy.get('#username').type('June')
       cy.get('#password').type('salainen')
       cy.get('#login-button').click()
-      cy.contains('June logged-in')
+      cy.contains('June logged-in').should('be.visible')
     })
 
     it('fails with wrong credentials', function() {
       cy.get('#username').type('June')
       cy.get('#password').type('xxxxx')
       cy.get('#login-button').click()
-      cy.contains('Wrong username or password')
+      cy.contains('June logged-in').should('not.exist')
+      cy.get('.error')
+       .should('contain', 'Wrong username or password')
+       .and('have.css', 'color', 'rgb(255, 0, 0)')
+    })
+  })
+  describe('When logged in', function() {
+    beforeEach(function() {
+      cy.login({ username: 'June', password: 'salainen' })
+    })
+    //All changes to the browser's state are reversed after each test.
+
+    it('A blog can be created', function() {
+      cy.createBlog(
+        { title: 't-test', author: 'a-test', url: 'http://example.com' }
+      )
+      cy.contains('A new blog "t-test" by a-test added')
+      cy.contains('t-test a-test').should('be.visible')
+    })
+  })
+  describe('when a blog exists',function(){
+    beforeEach(function() {
+      cy.login({ username: 'June', password: 'salainen' })
+      cy.createBlog(
+        { title: 't-test', author: 'a-test', url: 'http://example.com' }
+      ) 
+      cy.visit('')
+    })
+    it ('Users can like a blog',function(){
+      cy.contains('t-test a-test').parent().contains('View').click()
+      cy.contains('likes').then($p => {
+        const text = $p.text()           
+        const currentLikes = Number(text.match(/\d+/)[0])
+        cy.get('[aria-label="like-button"]').click()
+        cy.contains('likes').should('contain', `likes ${currentLikes + 1}`)
+      })
+    })
+  })
+  describe('blog deletion', function() {
+    beforeEach(function() {
+      cy.login({ username: 'June', password: 'salainen' })
+      cy.createBlog(
+        { title: 't-test', author: 'a-test', url: 'http://example.com' }
+      ) 
+      cy.visit('')
+    })
+    it ('A user who created a blog can delete it', function() {
+      cy.contains('t-test a-test').parent().contains('View').click()
+      cy.get('[aria-label="remove-button"]').click()
+      cy.contains('t-test a-test').should('not.exist')
+    })
+    it('other users cannot delete the blog', function() {
+      cy.get('[aria-label="logout-button"]').click()
+      const anotherUser={
+        name:'July',
+        username:'July',
+        password:'salainen'
+      }
+    cy.request('POST', `${Cypress.env('BACKEND')}/users`, anotherUser)
+    cy.login({ username: 'July', password: 'salainen' })   
+    cy.visit('')
+    cy.contains('t-test a-test').parent().contains('View').click()
+    cy.get('[aria-label="remove-button"]').should('not.exist')
     })
   })
 })
-
