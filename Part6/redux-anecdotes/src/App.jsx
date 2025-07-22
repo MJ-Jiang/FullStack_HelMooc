0736ab1b99fb123147a8fd211a216890640929cb
MJ-Jiang <maura_jiang@outlook.com>
2025-07-22 11:19:45 +0300

    6.14

--- Part6/redux-anecdotes/src/App.jsx
+++ Part6/redux-anecdotes/src/App.jsx
@@ -2,7 +2,15 @@ import AnecdoteForm from './components/AnecdoteForm'
 import AnecdoteList from './components/AnecdoteList'
 import VisibilityFilter from './components/VisibilityFilter'
 import Notification from './components/Notification'
+import {initializeAnecdotes} from './reducers/anecdoteReducer'
+import { useEffect } from 'react'
+import { useDispatch } from 'react-redux'
+
 const App = () => {
+  const dispatch = useDispatch()
+  useEffect(() => {
+   dispatch(initializeAnecdotes())
+  }, [dispatch])
   return (
     <div>
 
