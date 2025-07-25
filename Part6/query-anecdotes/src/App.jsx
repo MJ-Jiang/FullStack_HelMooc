1b0d6f83d63b7dde3ea46272f069e222e2f2a8f2
MJ-Jiang <maura_jiang@outlook.com>
2025-07-25 14:36:03 +0300

    6.20

--- Part6/query-anecdotes/src/App.jsx
+++ Part6/query-anecdotes/src/App.jsx
@@ -1,20 +1,28 @@
 import AnecdoteForm from './components/AnecdoteForm'
 import Notification from './components/Notification'
-
+import { useQuery } from '@tanstack/react-query'
+import { getAnecdotes } from './requests'
 const App = () => {
 
+  const {data,isLoading,isError}=useQuery({
+    queryKey:['anecdotes'],
+    queryFn:getAnecdotes,
+    retry:false
+  })
+  if (data) {
+  console.log(JSON.parse(JSON.stringify(data)))
+ }
+  if(isLoading){
+    return <div>loading data...</div>
+  }
+  if(isError){
+    return <div>Anecdote service not available due to problems in server</div>
+  }
+
+  const anecdotes = data
   const handleVote = (anecdote) => {
     console.log('vote')
   }
-
-  const anecdotes = [
-    {
-      "content": "If it hurts, do it more often",
-      "id": "47145",
-      "votes": 0
-    },
-  ]
-
   return (
     <div>
       <h3>Anecdote app</h3>
