64dde13fa4ba83686882c677619c65d192791a2c
MJ-Jiang <maura_jiang@outlook.com>
2025-07-28 10:21:30 +0300

    7.4

--- Part7/routed-anecdotes/src/App.jsx
+++ Part7/routed-anecdotes/src/App.jsx
@@ -3,6 +3,9 @@ import {
   BrowserRouter as Router,
   Routes, Route, Link, useMatch,useNavigate
 } from 'react-router-dom'
+import CreateNew from './components/CreateNew'
+
+
 const Menu = () => {
   const padding = {
     paddingRight: 5
@@ -64,45 +67,7 @@ const Footer = () => (
   </div>
 )
 
-const CreateNew = (props) => {
-  const [content, setContent] = useState('')
-  const [author, setAuthor] = useState('')
-  const [info, setInfo] = useState('')
- const navigate=useNavigate()
 
-  const handleSubmit = (e) => {
-    e.preventDefault()
-    props.addNew({
-      content,
-      author,
-      info,
-      votes: 0
-    })
-    navigate('/')
-  }
-
-  return (
-    <div>
-      <h2>create a new anecdote</h2>
-      <form onSubmit={handleSubmit}>
-        <div>
-          content
-          <input name='content' value={content} onChange={(e) => setContent(e.target.value)} />
-        </div>
-        <div>
-          author
-          <input name='author' value={author} onChange={(e) => setAuthor(e.target.value)} />
-        </div>
-        <div>
-          url for more info
-          <input name='info' value={info} onChange={(e)=> setInfo(e.target.value)} />
-        </div>
-        <button>create</button>
-      </form>
-    </div>
-  )
-
-}
 
 const App = () => {
   const [anecdotes, setAnecdotes] = useState([
@@ -154,8 +119,7 @@ const App = () => {
             <Menu />
             {notification && (
               <div style={{ border: '1px solid red'}}>{notification}
-              </div>
-  )}
+              </div>)}
 
             <Routes>
                 <Route path="/" element={<AnecdoteList anecdotes={anecdotes} />} />
